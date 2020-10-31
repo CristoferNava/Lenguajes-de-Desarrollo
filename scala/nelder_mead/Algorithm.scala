e87bb9bc4084eb472ec5cd40883f08dec8cad91c
CristoferNava <cj.carmonanava@ugto.mx>
2020-10-31 17:07:19 -0600

    sort working

--- scala/nelder_mead/Algorithm.scala
+++ scala/nelder_mead/Algorithm.scala
@@ -3,9 +3,10 @@ object Algorithm {
         val v1 = new Point2D(0, 0)        
         val v2 = new Point2D(1.2, 0)
         val v3 = new Point2D(0, 0.8)
-
+        
+        def f(v: Point2D): Double = (v.x*v.x) - (4*v.x) + (v.y*v.y) - (v.y) - (v.x*v.y)
         def orden(p1: Point2D, p2: Point2D): Boolean = {
-            if (p1.z > p2.z)
+            if (f(p1) > f(p2))
                 return false
             return true
         }
