03c5af740788de49410ebdd0f4b1bcf1923c5812
CristoferNava <cj.carmonanava@ugto.mx>
2020-11-02 09:23:04 -0600

    comments were added

--- scala/nelder_mead/Main.scala
+++ scala/nelder_mead/Main.scala
@@ -1,13 +1,16 @@
 object Main {
   def main(args: Array[String]): Unit = {
+    // definimos las funciones que vamos a evaluar
     val sphere = (v: Point2D) => (v.x*v.x) + (v.y*v.y)
     val example = (v: Point2D) => (v.x*v.x) - (4*v.x) + (v.y*v.y) - (v.y) - (v.x*v.y)
     val rosenbrock = (v: Point2D) => (1-v.x) + 100*((v.y-(v.x*v.x))*(v.y-(v.x*v.x)))
 
+    // pasamos dichas funciones al algoritmo
     val minSphere: Point2D = Algorithm.nelderMead(sphere)
     val minExample: Point2D = Algorithm.nelderMead(example)
     val minRosenbrock: Point2D = Algorithm.nelderMead(rosenbrock)
 
+    // mínimos encontrados
     println(f"Sphere: $minSphere")
     println(f"Example: $minExample")
     println(f"Rosenbrock: $minRosenbrock")
